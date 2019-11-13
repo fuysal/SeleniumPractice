4b04e1481dd8cc374fafeed13ede046b8e6fcfa6
shmammadov <mr.shmammadov@gmail.com>
2019-11-13 01:53:26 -0500

    explicit wait finished

--- src/test/java/com/cybertek/Day12_waits/ExpilicitWaitTest.java
+++ src/test/java/com/cybertek/Day12_waits/ExpilicitWaitTest.java
@@ -1,10 +1,13 @@
 package com.cybertek.Day12_waits;
 
 import com.cybertek.Utilities.BrowserFactory;
+import com.cybertek.Utilities.SeleniumUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,6 +20,10 @@ public class ExpilicitWaitTest {
     String usernameLocator = "#username";
     String passwordLocator = "#pwd";
     String submitLocator = "[type='submit']";
+    String loadingLocator = "#loading";
+    String removeLocator = "//button[text()='Remove'] ";
+    String enableLocator  = "#input-example>[type='button']";
+    String addLocator = "//button[text()='Add']";
 
 
     WebDriver driver;
@@ -26,7 +33,7 @@ public class ExpilicitWaitTest {
         driver = new ChromeDriver();
         //driver = BrowserFactory.getDriver("chrome");
 
-        driver.manage().timeouts().implicitlyWait(  10, TimeUnit.SECONDS);
+      driver.manage().timeouts().implicitlyWait(  10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
     }
@@ -34,13 +41,47 @@ public class ExpilicitWaitTest {
     public void waitForVisibilityTest(){
         driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
         driver.findElement(By.cssSelector(startButtonLocator)).click();
+        System.out.println(driver.findElement(By.cssSelector(loadingLocator)).getText() );
+
+
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        //wait until input box is visible
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector(usernameLocator))));
         driver.findElement(By.cssSelector(usernameLocator)).sendKeys("tomsmith ");
         driver.findElement(By.cssSelector(passwordLocator)).sendKeys("SuperSecretPassword ");
+
         driver.findElement(By.cssSelector(submitLocator)).click();
 
 
     }
 
+    @Test
+    public void waitForElementToBeClickable(){
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        driver.findElement(By.xpath(removeLocator)).click();
+        //SeleniumUtils.waitPlease(2);
+        driver.findElement(By.cssSelector(addLocator)).click();
+    }
+    @Test
+    public void waitForElementToBeClickable2() {
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+        driver.findElement(By.xpath(removeLocator)).click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(addLocator))));
+        //SeleniumUtils.waitPlease(2);
+        driver.findElement(By.xpath(addLocator)).click();
+    }
+        @Test
+        public void waitForElementToBeClickable3(){
+            driver.get("http://practice.cybertekschool.com/dynamic_loading/5");
+
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.elementToBeClickable(  driver.findElement(By.cssSelector(submitLocator))));
+            driver.findElement(By.cssSelector(submitLocator)).click();
+
+
+    }
     @AfterMethod
     public void tearDown(){
         driver.quit();
